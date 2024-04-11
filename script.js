5149d6cf2e78eb360be14c047bd97f121de0c99b
AbuTalha3 <najeebakhun@gmail.com>
2024-04-11 11:16:53 +0500

    Add step 9

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
+
+
   console.log(numberInput.value);
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", () => {
-  
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
