e5eaa4eb8e9e8fde6e5ad6783429067163c42636
AbuTalha3 <najeebakhun@gmail.com>
2024-04-12 13:06:28 +0500

    Add js code step 20

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  return "01110110";
+};
+
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
     alert("Please provide a decimal number");
-    
+    return;
   }
 
-  console.log(numberInput.value);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
