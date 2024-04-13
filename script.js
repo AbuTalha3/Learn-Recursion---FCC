8c1c8bbc86849308d859dc311d725a5fe26a7d94
AbuTalha3 <najeebakhun@gmail.com>
2024-04-13 11:27:44 +0500

    remove everything in the body of the decimalToBinary function. Then, use let to create a variable named binary and assign it an empty string.

--- script.js
+++ script.js
@@ -3,30 +3,9 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = "";
 
-  if (input === 0) {
-    result.innerText = "0";
-    return;
-  }
-
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
-
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
-
-  result.innerText = remainders.reverse().join("");
+  
 };
 
 const checkUserInput = () => {
