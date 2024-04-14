0fde11d810f102a5d18cdcfd08b8e3eee4de20d5
AbuTalha3 <najeebakhun@gmail.com>
2024-04-14 11:39:23 +0500

    Modify code remove stack function 50% step 55

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-const callStack = [
-  'a(): returns "freeCodeCamp " + b()',
-  'b(): returns "is " + c()'
-];
-
-const a = () => {
-  return "freeCodeCamp " + b();
-};
-
-const b = () => {
-  return "is " + c();
-};
-
-const c = () => {
-  return "awesome!";
-};
-
-console.log(a());
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+
 
 const decimalToBinary = (input) => {
   let binary = "";
