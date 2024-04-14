ae13a5f068e7968d40b2aa4ff746f49d5e1a513b
AbuTalha3 <najeebakhun@gmail.com>
2024-04-14 12:54:04 +0500

    remove countupdown function and content of decimaltobinary func

--- script.js
+++ script.js
@@ -2,33 +2,8 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countDownAndUp = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    console.log("Reached base case");
-    return;
-  } else {
-    countDownAndUp(number - 1);
-    console.log(number);
-  }
-};
-
-countDownAndUp(3);
-
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
-  }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
+  
 };
 
 const checkUserInput = () => {
