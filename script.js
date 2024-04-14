62746d6a194b2da46dacf441c20679a7b7b03eec
AbuTalha3 <najeebakhun@gmail.com>
2024-04-14 15:03:54 +0500

    Modify js step 90

--- script.js
+++ script.js
@@ -29,15 +29,10 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
-  setTimeout(() => {
-    console.log("free");
-  }, 500);
-  setTimeout(() => {
-    console.log("Code");
-  }, 1000);
-  setTimeout(() => {
-    console.log("Camp");
-  }, 1500);
+  result.innerText = "Call Stack Animation";
+  animationData.forEach((obj) => {
+    
+  });
 };
 
 const checkUserInput = () => {
