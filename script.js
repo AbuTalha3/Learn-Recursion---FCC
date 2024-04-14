f55dc3c09c0ad59a37b0c7233636f5d150372212
AbuTalha3 <najeebakhun@gmail.com>
2024-04-14 19:33:25 +0500

    step 105

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const animationData = [
   {
     inputVal: 5,
     marginTop: 300,
-    addElDelay: 1000
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
   },
   {
     inputVal: 2,
     marginTop: -200,
-    addElDelay: 1500
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
   },
   {
     inputVal: 1,
@@ -42,6 +48,12 @@ const showAnimation = () => {
         </p>
       `;
     }, obj.addElDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+
+
   });
 };
 
